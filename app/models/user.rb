f34db317865fd5abc1e615e7137f910f58ec666e
bateso88 <bateso@hotmail.co.uk>
2021-04-20 17:13:55 +0100

    Add company name to user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :listings
+  has_many :listings, dependent: :destroy
 end
