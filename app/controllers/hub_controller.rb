33a9937e87dac9be7238006a67feba3cf6a51e5a
bateso88 <bateso@hotmail.co.uk>
2021-04-21 16:24:24 +0100

    Add Hub page

---
+++ app/controllers/hub_controller.rb
@@ -0,0 +1,8 @@
+class HubController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  end
+
+  def site_appraisal_tool
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
   # get 'home/index'
-  get 'home/about'
   root 'home#index'
+  get 'home/about'
+  get 'hub/index'
+  get 'hub/site_appraisal_tool'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
