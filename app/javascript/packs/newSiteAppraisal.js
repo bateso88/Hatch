a0019dbc9946c42f450a20c05c3c48c9ca328597
bateso88 <bateso@hotmail.co.uk>
2021-05-05 17:42:14 +0100

    Refactor vanilla javascript to jquery

--- app/javascript/packs/newSiteAppraisal.js
+++ app/javascript/packs/newSiteAppraisal.js
@@ -1,32 +1,14 @@
 $(function () {
   /* event listeners */
-  document.getElementById("lease-length").addEventListener('change', addRentInputs);
-  document.getElementById("fixed-or-rising").addEventListener('change', alertWithChoice);
-  
-  /* functions */
+  $("#lease-length").change(addRentInputs);
+  $("#fixed-or-rising").change(alertWithChoice);
+
   function alertWithChoice() {
      alert('You chose ' + this.value + '!');
   }
 
   function addRentInputs() {
-
-    var formField = document.createElement("div");
-    formField.setAttribute("class", "form-floating mb-1");
-
-    var rentInput = document.createElement("input");
-    rentInput.setAttribute("type", "number");
-    rentInput.setAttribute("class", "form-control");
-    rentInput.setAttribute("placeholder", 0);
-    rentInput.setAttribute("id", "rent-year-1");
-
-    var label = document.createElement("label");
-    label.setAttribute("for", "rent-year-1");
-    label.innerHTML = `Rent Year ${this.value}`;
-
-    var variableTerms = document.getElementById("variable-terms");
-    variableTerms.appendChild(formField);
-    formField.appendChild(rentInput);
-    formField.appendChild(label);
+    $("#variable-terms").append("<div class='form-floating mb-1 rent-year-form'></div>");
+    $(".rent-year-form").last().append(`<input type="number" class="form-control" placeholder=0></input><label>Rent Year ${this.value}</label>`);
   }
-
 });
