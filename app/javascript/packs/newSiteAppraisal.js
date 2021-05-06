5b40848a9b12e9f6e63aaa3344254616973468a7
bateso88 <bateso@hotmail.co.uk>
2021-05-06 11:53:24 +0100

    rent inputs change asyncronously depending on previous inputs

--- app/javascript/packs/newSiteAppraisal.js
+++ app/javascript/packs/newSiteAppraisal.js
@@ -1,15 +1,21 @@
 $(function () {
   // Event listeners 
-  $("#lease-length").change(addRentInputs);
-  $("#fixed-or-rising").change(alertWithChoice);
+  $("#lease-length").on("change", addRentInputs);
+  $("#fixed-or-rising").on("change", addRentInputs);
 
-  // Callbacks
-  function alertWithChoice() {
-     alert('You chose ' + this.value + '!');
-  }
-
+  // Callback
   function addRentInputs() {
-    $("#variable-terms").append("<div class='form-floating mb-1 rent-year-form'></div>");
-    $(".rent-year-form").last().append(`<input type="number" class="form-control" placeholder=0></input><label>Rent Year ${this.value}</label>`);
+    $("#rent-inputs").remove();
+    $("#variable-terms").append("<div id='rent-inputs'></div>");
+
+    if($("#fixed-or-rising").val() === "Rising") {
+      for(let i = 1; i<=$("#lease-length").val(); i++){
+        $("#rent-inputs").append("<div class='form-floating mb-1 rent-year-form'></div>");
+        $(".rent-year-form").last().append(`<input type="number" class="form-control" placeholder=0></input><label>Rent Year ${i}</label>`);
+      }
+    } else {
+      $("#rent-inputs").append("<div class='form-floating mb-1 rent-year-form'></div>");
+      $(".rent-year-form").last().append(`<input type="number" class="form-control" placeholder=0></input><label>Annual Rent</label>`);
+    }
   }
 });
