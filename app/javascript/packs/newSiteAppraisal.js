b6f31745f085b6384cdf8feb8eedee02b8108b58
bateso88 <bateso@hotmail.co.uk>
2021-05-06 17:16:05 +0100

    Add dynamically changing number of fields for projections

--- app/javascript/packs/newSiteAppraisal.js
+++ app/javascript/packs/newSiteAppraisal.js
@@ -1,39 +1,60 @@
 $(function () {
   $(".rent-year-form").hide()
+  $(".projection-form").hide()
   $("#annual-rent").show();
+  $("#projection-year-1").show();
 
   // Event Listeners 
-  $("#lease-length").on("change", updateRentInputs);
+  $("#lease-length").on("change", updateRentInputsAndProjections);
   $("#fixed-or-rising").on("change", updateRentInputs);
   $("#variable-terms").on('change', updateTotalRent);
+  $("#projections").on('change', updateTotalProjections);
 
   // Callbacks
+
+  function updateRentInputsAndProjections() {
+    updateRentInputs();
+    $(".projection-form").hide()
+    for(let i = 1; i<=$("#lease-length").val(); i++){
+      $(`#projection-year-${i}`).show("slow");
+    }
+  };
+
+
   function updateRentInputs() {
     $(".rent-year-form").hide()
     if($("#fixed-or-rising").val() === "Rising") {
       showRentYearFields();
     } else {
-      $("#annual-rent").show();
+      $("#annual-rent").show("slow");
     }
   }
 
   function showRentYearFields() {
     for(let i = 1; i<=$("#lease-length").val(); i++){
-      $(`#rent-year-${i}`).show();
+      $(`#rent-year-${i}`).show("slow");
     }
   }
 
   function updateTotalRent() {
-    let rentArray = [];
+    let rent = [];
     if($("#fixed-or-rising").val() === "Rising") {
       for(let i = 1; i<=$("#lease-length").val(); i++){
-        rentArray.push($(`#rent-year-${i} input`).val())
+        rent.push($(`#rent-year-${i} input`).val())
       }
     } else {
       for(let i = 1; i<=$("#lease-length").val(); i++){
-        rentArray.push($(`#annual-rent input`).val())    
+        rent.push($(`#annual-rent input`).val())    
       }
     }
-    $("#overall-rent").val(rentArray)
+    $("#overall-rent").val(rent)
+  }
+
+  function updateTotalProjections() {
+    let projections = []
+    for(let i = 1; i<=$("#lease-length").val(); i++){
+      projections.push($(`#projection-year-${i} input`).val())
+    }
+    $("#overall-projections").val(projections)
   }
 });
